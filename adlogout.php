790f0cfb30beda4c48075c576aa2f53e19679727
MrBobbyX <107630676+MrBobbyX@users.noreply.github.com>
2023-03-19 14:28:08 +0800

    Update adlogout.php

--- adlogout.php
+++ adlogout.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (!isset($_SESSION['id']))///Entire If statement is the changes made --> Ahmad
+{
+    header("location:logout.php");
+}//End of IF statement
 session_destroy();
 header("Location: adminlogin.php");
 ?>
